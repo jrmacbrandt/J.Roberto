8756b1e730aaf0393bd32d110931a3dc31f49149
J. Roberto <jrmacbrandt@gmail.com>
2026-05-05 17:07:00 -0300

    style: shrink hero image to 41% to avoid header overlap

--- src/App.tsx
+++ src/App.tsx
@@ -256,7 +256,7 @@ const Header = () => {
       </div>
 
       {/* Lado Direito: Imagem de Perfil (Apenas Desktop) - Alinhada à borda direita com 0 espaçamento */}
-      <div className="hidden md:block absolute bottom-0 right-0 w-[44%] pointer-events-none select-none z-10">
+      <div className="hidden md:block absolute bottom-0 right-0 w-[41%] pointer-events-none select-none z-10">
         <img 
           src="/profile.png" 
           alt="J. Roberto Brandt" 
